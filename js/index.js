d8d03f4435211aba5fd39679cb2b4d8e3b587d0e
5mk00 <omk000915@gmail.com>
2023-06-29 05:08:47 +0900

    moblie benefit, brand css

--- js/index.js
+++ js/index.js
@@ -1,13 +1,15 @@
-//navBar
+// navBar 스크롤 될 때 흰생 배경 넣기
 // mainPage > Daily moisture care 부분 스크롤시 이미지 위로 움직임
-// mainPage > 마지막 부분 밑줄
-// productBottomBar
+// mainPage > 마지막 부분 밑줄 스크롤 시 생김
+
 // * accordion 만들기
-// 색상, ml 선택
+// 색상, ml 선택 시 이미지, 설명, 버튼 변경
+// detailPage Read More 누르면 문장 이어붙이기
 // showPW, forgotPW error, submit
-//.accComponent
+//.accComponent active
 // toggle on/off
-//inputTabPage
+//custormerInquiry inputTab 선택 => 해당되는 inputTabPage 보이기
+
 var reloadTarget = null;
 
 $(document).ready(function(){
@@ -20,9 +22,21 @@ $(document).ready(function(){
     accActive();
     panelControl(".shopBtn input[type='button']");
     panelControl(".btnSubmit");
-    asideScroll();
-    periodSearch();
+    scrollEffect();
 });
+function scrollEffect(){
+    var effLine = 0;
+    var currentPos = 0;
+    effLine = $(".scrollTarget").offset().top + 50;
+    $(window).scroll(function(){
+        currentPos = $(this).scrollTop();
+        if(currentPos >= effLine){
+            $(".scrollTarget").addClass("active");
+        }else{
+            $(".scrollTarget").removeClass("active");
+        }
+    });
+}
 function panelControl(openBtn){
     var currentPanelName = null;
     $(openBtn).click(function(){
@@ -100,22 +114,23 @@ function accActive(){
 
 
 
-
-function asideScroll(){
-	var left = $('div > aside ul li a.active').offset().left - $('div > aside ul li').offset().left;
-    var curLeft = $('div > aside ul').scrollLeft();          
-    $('div > aside ul').scrollLeft(curLeft + left);
-}
-
-
+/* 모바일 메뉴 좌우스크롤에서 선택된 메뉴 쪽으로 스크롤 되도록 하기*/ 
+// function asideScroll(){
+// 	var left = $('div > aside ul li a.active').offset().left - $('div > aside ul li').offset().left;
+//     var curLeft = $('div > aside ul').scrollLeft();          
+//     $('div > aside ul').scrollLeft(curLeft + left);
+// }
 
 
-function periodSearch(){
-    $('.shippingOrder > div > div > div > div:first-of-type > label').click(function(){
-        $('.shippingOrder > div > div > div > div:first-of-type > *:not(:first-child), .shippingOrder > div > div > div > input').toggleClass('open');
-    });
-    $('.shippingOrder > div > div > div > input').click(function(){
-        $('.shippingOrder > div > div > div > input').removeClass('active');
-        $(this).toggleClass('active');
-    });
-}
+
+/* 모바일 shopping&order에서 구매 기간 조회 펼치기 
++ 버튼 active*/
+// function periodSearch(){
+//     $('.shippingOrder > div > div > div > div:first-of-type > label').click(function(){
+//         $('.shippingOrder > div > div > div > div:first-of-type > *:not(:first-child), .shippingOrder > div > div > div > input').toggleClass('open');
+//     });
+//     $('.shippingOrder > div > div > div > input').click(function(){
+//         $('.shippingOrder > div > div > div > input').removeClass('active');
+//         $(this).toggleClass('active');
+//     });
+// }
